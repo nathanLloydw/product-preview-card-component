5485810c2f902de217871e9eee5b0bbbaf24120b
nlloydw <nlloydw61@gmail.co.uk>
2023-02-03 13:58:59 +0000

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-        
+        colors:
+            {
+                'dark-cyan':'hsl(158, 36%, 37%)',
+                'cream':'hsl(30, 38%, 92%)',
+                'v-dark-blue':'hsl(212, 21%, 14%)',
+                'dark-grayish-blue':'hsl(228, 12%, 48%)',
+            }
     },
   },
   plugins: [],
